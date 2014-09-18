f46484aab91e8b3e977ee333234e1989bba8bf4a
EffendiConsulting <effendiconsulting@gmail.com>
2014-09-18 10:51:54 -0500

    changed code on processed_invoice_file_sizes.rb

--- processed_invoice_file_sizes.rb
+++ processed_invoice_file_sizes.rb
@@ -4,6 +4,18 @@
 #   end
 # end
 
-Dir['/Users/Jason/Desktop/refund_retriever/2014-09-13/*.csv'].each do |fname|
+# Dir['/Users/Jason/Desktop/refund_retriever/2014-09-13/*.csv'].each do |fname|
+#   puts "#{fname} #{File.open(fname, 'r').readlines.length}"
+# end
+
+path = '../invoice-auditor/' # relative path from current file to
+#                              required folder
+
+Dir["File.dirname(__FILE__) + '/' + path + '*.csv'"].each do |fname|
+  require_relative path + File.basename(fname) # require all files with .csv
+  #                                              extension in this folder
   puts "#{fname} #{File.open(fname, 'r').readlines.length}"
 end
+
+# def invoice_info
+#
