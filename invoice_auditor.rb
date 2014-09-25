6a2d9f0cc498b0b40e0eb648bbd718f3c95dbf3f
EffendiConsulting <effendiconsulting@gmail.com>
2014-09-25 11:22:10 -0500

    Changes and additions to invoice_auditor.rb

--- invoice_auditor.rb
+++ invoice_auditor.rb
@@ -1,27 +1,5 @@
-# InvoiceData takes a given folder and cycles through its invoices, returning
-#
-# class InvoiceData
-#   def initialize
-#     @invoice_info = {}
-#     @csv_filename = ''
-#     @invoice_folder = ''
-#   end
-#
-#   def parse_invoice_data
-#     @invoice_info = { file_name: @csv_filename,
-#                       line_count: File.open(@csv_filename, 'r').readlines.length
-#                     }
-#   end
-#
-#   def pull_invoice_data
-#     Dir[@invoice_folder].each do |csv_filename|
-#       parse_invoice_data(@csv_filename)
-#     end
-#   end
-# end
-
+# Takes each invoice file and prints the filename and linecount.
 class Invoice
-
   attr_reader :csv_filename, :linecount
 
   def initialize(csv_filename)
@@ -35,8 +13,11 @@ class Invoice
   end
 end
 
+# Takes the folder location and, for each invoice file within,
+# and parses the filename and linecount, assigning them variables.
 class InvoiceFolder
   attr_reader :invoices
+
   def initialize(folder)
     @folder = folder
     @invoices = []
@@ -54,20 +35,10 @@ class InvoiceFolder
   end
 end
 
-# def invoice_array_output
-#   invoices_array = []
-#   invoices_array.each do |invoices|
-#     pull_invoice_data(ARGV.first)
-#     # invoices_array.push { invoice_info }
-#     invoices_array << invoice_info
-#   end
-#   puts invoices_array
-# end
-
 invoice_folder = InvoiceFolder.new(ARGV.first)
 invoice_folder.process_invoices
 invoice_folder.invoices.each do |invoice|
   puts "Invoice file: #{invoice[:filename]}"
   puts "Linecount: #{invoice[:linecount]}"
-  puts "*********************"
+  puts '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 end
